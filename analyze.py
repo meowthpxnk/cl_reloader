539f39007cc787964124e126757935f2de80a081
MeowthPxnk <meowthpxnk@gmail.com>
2024-09-26 15:01:08 +0300

    123

--- analyze.py
+++ analyze.py
@@ -2,30 +2,40 @@ from cl_getter import get_clickers
 from iter_cls import iter_cls
 from reloader import reload_device
 
+
 async def analyze(DB_PATH):
     clickers = get_clickers(DB_PATH)
     data = await iter_cls(clickers)
 
     for res in data:
         if res.get("error"):
-            print(f"Error with device {res['device']['phone']} Error: {res.get("error")}")
+            print(
+                f"Error with device {res['device']['phone']} Error: {res.get('error')}"
+            )
             continue
         status = res["req"]["status"]
         if status == 502:
-            print(f"{res['device']['phone']} Should be reloaded on server {res['device']['server']}, port {[res['device']['port']]}")
+            print(
+                f"{res['device']['phone']} Should be reloaded on server {res['device']['server']}, port {[res['device']['port']]}"
+            )
             try:
-                reload_device(res['device']['phone'])
+                reload_device(res["device"]["phone"])
             except Exception as error:
-                print(f"Failed reload device with phone {res['device']['phone']}, Error: {error}")
+                print(
+                    f"Failed reload device with phone {res['device']['phone']}, Error: {error}"
+                )
             finally:
                 continue
         elif status == 404:
             print(f"Alright with phone {res['device']['phone']}")
             continue
-        print(f"🤷‍♀️🤷‍♀️🤷‍♀️, Phone {res['device']['phone']} statuscode is {status}")
+        print(
+            f"🤷‍♀️🤷‍♀️🤷‍♀️, Phone {res['device']['phone']} statuscode is {status}"
+        )
 
 
 if __name__ == "__main__":
     from get_db_path import get_db_path
     import asyncio
+
     asyncio.run(analyze(get_db_path()))
